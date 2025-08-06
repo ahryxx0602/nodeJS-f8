60d4c4c266da48e30b4b41235a66b121394dc123
Ahryxx <phvanthanh06@gmail.com>
2025-08-06 09:00:57 +0700

    Install pettier lint-staged huskyhusky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,13 @@
 class SiteController {
-  //[GET] /news
-  index(req, res) {
-    res.render("home");
-  }
+    //[GET] /news
+    index(req, res) {
+        res.render('home');
+    }
 
-  //[GET] /news/:slug
-  search(req, res) {
-    res.render("search");
-  }
+    //[GET] /news/:slug
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
